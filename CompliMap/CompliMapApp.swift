99ee03db97726be42a0679c960ef152ad8b4003a
Sam Christensen <samctensen@gmail.com>
2022-05-07 16:16:08 -0600

    Basic Xcode setup

---
+++ CompliMap/CompliMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompliMapApp.swift
+//  CompliMap
+//
+//  Created by Sam Christensen on 5/4/22.
+//
+
+import SwiftUI
+
+@main
+struct CompliMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
