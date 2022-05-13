88a7b7afdf44d28034d9cb3bac5d0b2f331e6950
Sam Christensen <samctensen@gmail.com>
2022-05-12 20:03:03 -0600

    Added basic Compliment and Profile classes

---
+++ CompliMap/Compliment.swift
@@ -0,0 +1,21 @@
+//
+//  Compliment.swift
+//  CompliMap
+//
+//  Created by Sam Christensen on 5/12/22.
+//
+
+import Foundation
+class Compliment {
+    private var text: String
+    private var timeStamp: Int
+    private var geoTag: String
+    private var numVotes: Int
+    
+    init(text:String, timeStamp:Int, geoTag:String, numVotes:Int) {
+        self.text = ""
+        self.timeStamp = 0
+        self.geoTag = ""
+        self.numVotes = 0
+    }
+}
